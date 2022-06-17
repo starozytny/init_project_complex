b65881c47d86e380fd11abe2e57cea63bfbed6e8
Shanbora <chanbora@logilink.fr>
2022-06-17 16:33:33 +0200

    tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     plugins: {
-        autoprefixer: ({grid: "true"})
+        tailwindcss: {},
+        autoprefixer: {},
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "templates/**/*.html.twig",
+      "assets/app/js/**/*.js",
+      "assets/app/js/**/*.jsx",
+      "assets/common/js/**/*.js",
+      "assets/common/js/**/*.jsx",
+      "assets/dashboard/js/**/*.js",
+      "assets/dashboard/js/**/*.jsx",
+      "assets/user/js/**/*.js",
+      "assets/user/js/**/*.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
